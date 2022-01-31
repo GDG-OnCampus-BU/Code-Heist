cf34dbe83a5c8401ac763403633e00c097da8811
Siva Prakash K <56880935+siva010928@users.noreply.github.com>
2022-01-31 16:53:09 +0530

    Update solution.cpp

--- contest2/div1/ques2/solution.cpp
+++ contest2/div1/ques2/solution.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+//which two sticks will make pair to collect maximum volume of water
+#include<bits/stdc++.h>
+using namespace std;
 
 int main(){
     int t;
@@ -6,22 +8,29 @@ int main(){
     while(t-->0){
         int n;
         cin>>n;
-        vector<int>  arr(n);
-        for(int i=0;i<n;i++) cin>>arr[i];
+        vector<int>  height(n);
+        for(int i=0;i<n;i++) cin>>height[i];
+        
+        cout<<solve(height)<<endl;
+    }
+}
+
+//to calculatate max area collected by a pair of sticks
+int solve(vector<int> height){
+        int n=height.size();
         int i=0,j=n-1;
         int maxArea=INT_MIN;
         while(i<j){
-            if(arr[i]<arr[j]){
-                maxArea=max(maxArea,(j-i)*arr[i]);
-                i++;
-            }else if(arr[i]>arr[j]){
-                maxArea=max(maxArea,(j-i)*arr[j]);
-                j--;
-            }else{
-                maxArea=max(maxArea,(j-i)*arr[i]);
-                i++;j--;
+            if(height[i]<height[j]){//when this occurs,ith stick being smaller wants to give maximum area by pairing with farest stick so it will pairup with jth
+                maxArea=max(maxArea,(j-i)*height[i]);
+                i++;//there is no way this ith stick give max area with other than pairing with this jth stick so we can eliminate ith stick
+            }else if(height[i]>height[j]){//when this occurs,jth stick being smaller wants to give maximum area by pairing with farest stick so it will pairup with ith
+                maxArea=max(maxArea,(j-i)*height[j]);
+                j--;//there is no way this jth stick give max area with other than pairing with this ith stick so we can eliminate jth stick
+            }else{//when both stick are same legnth
+                maxArea=max(maxArea,(j-i)*height[i]);
+                i++;j--;//here mutually both stick helps each other forming max area //there is no way that both sticks give max area with other than pairing with others so we can eliminate both sticks
             }
         }
-        cout<<maxArea<<endl;
-    }
+        return maxArea;
 }
